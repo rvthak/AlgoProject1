04eaf7aece9aba672365fccdba128b0ab00f2256
ChristosTsouchalris <ctsouch@outlook.com>
2021-10-25 13:53:35 +0300

    Added initialization of cube template function

--- include/Cube.h
+++ include/Cube.h
@@ -1,14 +1,25 @@
 #ifndef CUBE
 #define CUBE
 
-#include <vector>
+#define DEFAULT_DIMENSIONS_K 14
+#define DEFAULT_NEAREST_NEIGHBORS 1
+#define DEFAULT_VECTORS_TO_CHECK_M 10
+#define DEFAULT_PROBES 2
+#define DEFAULT_RADIOUS 1000
 
 typedef struct{
-	unsigned dimensions_k;
-  unsigned nearest_neighbors_N;
-  unsigned vectors_to_check_M;
-  unsigned probes;
-  unsigned radious;
+	int dimensions_k;
+  int nearest_neighbors_N;
+  int vectors_to_check_M;
+  int probes;
+  int radious;
 } Cube;
+
+
+Cube* create_cube(int dimensions_k = DEFAULT_DIMENSIONS_K,
+                  int nearest_neighbors_N = DEFAULT_NEAREST_NEIGHBORS,
+                  int vectors_to_check_M = DEFAULT_VECTORS_TO_CHECK_M,
+                  int probes = DEFAULT_PROBES,
+                  int radious = DEFAULT_RADIOUS);
 
 #endif
--- src/Cube.cpp
+++ src/Cube.cpp
@@ -1,1 +1,15 @@
 #include "Cube.h"
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+Cube* create_cube(int dimensions_k = DEFAULT_DIMENSIONS_K,
+                  int nearest_neighbors_N = DEFAULT_NEAREST_NEIGHBORS,
+                  int vectors_to_check_M = DEFAULT_VECTORS_TO_CHECK_M,
+                  int probes = DEFAULT_PROBES,
+                  int radious = DEFAULT_RADIOUS)
+{
+  return NULL;
+}
