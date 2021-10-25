19a95190309132918e6db75f79b594fbe567b838
ChristosTsouchalris <ctsouch@outlook.com>
2021-10-25 13:45:07 +0300

    Added input properties to cube

--- include/Cube.h
+++ include/Cube.h
@@ -4,7 +4,11 @@
 #include <vector>
 
 typedef struct{
-	unsigned id;
+	unsigned dimensions_k;
+  unsigned nearest_neighbors_N;
+  unsigned vectors_to_check_M;
+  unsigned probes;
+  unsigned radious;
 } Cube;
 
 #endif
