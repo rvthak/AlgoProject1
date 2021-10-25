61de7ff2439a7fff0e75b987cd7c336a7220f6e4
ChristosTsouchalris <ctsouch@outlook.com>
2021-10-25 13:58:59 +0300

    Created init & free methods for cube

--- include/Cube.h
+++ include/Cube.h
@@ -16,10 +16,12 @@ typedef struct{
 } Cube;
 
 
-Cube* create_cube(int dimensions_k = DEFAULT_DIMENSIONS_K,
-                  int nearest_neighbors_N = DEFAULT_NEAREST_NEIGHBORS,
-                  int vectors_to_check_M = DEFAULT_VECTORS_TO_CHECK_M,
-                  int probes = DEFAULT_PROBES,
-                  int radious = DEFAULT_RADIOUS);
+Cube* create_cube(int dimensions_k,
+                  int nearest_neighbors_N,
+                  int vectors_to_check_M,
+                  int probes,
+                  int radious);
+
+void free_cube(Cube* cube);
 
 #endif
--- src/Cube.cpp
+++ src/Cube.cpp
@@ -5,11 +5,24 @@
 
 using namespace std;
 
-Cube* create_cube(int dimensions_k = DEFAULT_DIMENSIONS_K,
-                  int nearest_neighbors_N = DEFAULT_NEAREST_NEIGHBORS,
-                  int vectors_to_check_M = DEFAULT_VECTORS_TO_CHECK_M,
-                  int probes = DEFAULT_PROBES,
-                  int radious = DEFAULT_RADIOUS)
+Cube* create_cube(int dimensions_k,
+                  int nearest_neighbors_N,
+                  int vectors_to_check_M,
+                  int probes,
+                  int radious)
 {
-  return NULL;
+  Cube* cube = new Cube;
+
+  cube->dimensions_k = dimensions_k;
+  cube->nearest_neighbors_N = nearest_neighbors_N;
+  cube->vectors_to_check_M = vectors_to_check_M;
+  cube->probes = probes;
+  cube->radious = radious;
+
+  return cube;
+}
+
+void free_cube(Cube* cube)
+{
+  delete cube;
 }
