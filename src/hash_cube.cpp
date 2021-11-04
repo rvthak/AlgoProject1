0b4ca6adf61e635e127764dbc242300a0e3ae366
Christos Tsouchlaris <ctsouch@outlook.com>
2021-11-04 10:09:49 +0200

    Updated main_cube & fixed bugs

--- src/hash_cube.cpp
+++ src/hash_cube.cpp
@@ -46,13 +46,16 @@ Hypercube::Hypercube(unsigned k, unsigned tableSize, unsigned v_size){
 Hypercube::~Hypercube(){
 	delete [] this->bucs;
 	delete this->f;
+	delete this->shorted_list;
 }
 
 
 // Add the given Vector to "this" Hash Table
 int Hypercube::add(Vector *vec)
 {
+	// vec->print();
 	unsigned key = this->f->hash(vec);
+
 	// cout << "Hash key : " << key << endl;
 	return (this->bucs)[key].add(vec, 0); // ID == 0 | We don't use ID for Cube
 }
@@ -64,7 +67,7 @@ void Hypercube::loadVectors(VectorArray *arr)
 		this->add(&((arr->array)[i]));
 	}
 
-	cout << "Loaded all the vectors at the cube!" << endl;
+	// cout << "Loaded all the vectors at the cube!" << endl;
 }
 
 void Hypercube::set_search_limits(unsigned probes, unsigned M, unsigned k)
@@ -79,7 +82,7 @@ int Hypercube::project_query_vector(Vector* query_vector)
 	unsigned projection_key = this->f->hash(query_vector);
 	// Bucket projection_bucket = (this->bucs)[projection_key];
 
-	cout << "Projection key : " << projection_key << endl;
+	// cout << "Projection key : " << projection_key << endl;
 
 	return projection_key;
 }
@@ -91,7 +94,7 @@ void Hypercube::analyze_query_vectors(VectorArray *query_vector_array)
 		this->project_query_vector(&((query_vector_array->array)[i]));
 	}
 
-	cout << "Analyzed all the query vectors at the cube!" << endl;
+	// cout << "Analyzed all the query vectors at the cube!" << endl;
 }
 
 int Hypercube::get_next_bucket_key(int last_bucket_key)
@@ -127,19 +130,31 @@ void Hypercube::iterate_bucket(Bucket* bucket, Vector* query_vector)
 {
 	Bucket_node *current_bucket_node = bucket->first;
 
+	if (current_bucket_node == nullptr) cout << "nullptr!!" << endl;
+
+	// cout << "Going to iterate bucket!" << endl;
+
 	while (current_bucket_node != nullptr)
 	{
+		// cout << "Entered loop" << endl;
 		// Get each vector of the bucket & the L2 distance to the query vector
 		Vector* vector = current_bucket_node->data;
 		double distance = query_vector->l2(vector);
 		this->shorted_list->add(vector, distance);
 		current_bucket_node = current_bucket_node->next;
 		this->vectors_searched++;
+		// cout << "Vectors searched : " << this->vectors_searched << endl;
 	}
 }
 
 ShortedList* Hypercube::search_hypercube(Vector *query)
 {
+	if (this->shorted_list != nullptr)
+	{
+		delete this->shorted_list;
+		this->shorted_list = new ShortedList(0);
+	}
+
 	this->probes_searched = 0;
 	this->vectors_searched = 0;
 	this->visited_bucket_keys = vector<int>();
@@ -153,21 +168,34 @@ ShortedList* Hypercube::search_hypercube(Vector *query)
 	int projection_key = this->project_query_vector(query);
 	Bucket* projection_bucket = &(this->bucs)[projection_key];
 
+	// cout << "Going to iterate bucket!" << endl;
+
 	this->iterate_bucket(projection_bucket, query);
 	last_key = projection_key;
 	visited_bucket_keys.push_back(last_key);
 
+	// cout << "Iterated bucket!" << endl;
+
 	while ((this->probes_searched <= max_probes) && (this->vectors_searched <= max_vectors))
 	{
 		int current_bucket_key = get_next_bucket_key(last_key);
-		Bucket* projection_bucket = &(this->bucs)[projection_key];
+		Bucket* projection_bucket = &(this->bucs)[current_bucket_key];
+		// Bucket* projection_bucket = &(this->bucs)[projection_key];
+
+		// cout << "Probes searched : " << this->probes_searched << endl;
+		// cout << "Vectors searched : " << this->vectors_searched << endl;
+
+		// cout << "Current bucket key : " << current_bucket_key << endl;
 
 		this->iterate_bucket(projection_bucket, query);
+		this->probes_searched++;
 		last_key = current_bucket_key;
 		visited_bucket_keys.push_back(last_key);
+
+		// cout << "Finished loop!" << endl;
 	}
 
-	cout << "Completed nearest neighbors search!" << endl;
+	// cout << "Completed hypercube search" << endl;
 
 	return this->shorted_list;
 }
--- src/main_Cube.cpp
+++ src/main_Cube.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
 #include "Args.h"
 #include "utils.h"
+#include "timer.h"
 #include "Vector.h"
+#include "List.h"
 #include "hash_cube.h"
+#include "shortedList.h"
+#include <iostream>
+#include <fstream>
+
+void report_results(std::string filename, unsigned id,
+	                  ShortedList *hypercube_results, double hypercube_time,
+	                  ShortedList *range_results);
 
 int main(int argc, char *argv[]){
 	bool running = true;
@@ -10,6 +18,14 @@ int main(int argc, char *argv[]){
 
 	// A struct to store the program parameters
 	ARGS_Cube args;
+
+	// Create a Timer to time the tests
+	Timer timer;
+	double hypercube_time;
+
+	// Pointers to the test results
+	ShortedList* hypercube_results;
+	ShortedList* range_results;
 
 	// Read the given terminal args (if any), and store any args you find
 	args.read_terminal(argc, argv);
@@ -26,21 +42,21 @@ int main(int argc, char *argv[]){
 		VectorArray input_vecs(args.input_file);
 		VectorArray query_vecs(args.query_file);
 
-
-
-		// query_vecs.print();
-
 		// Testing the cube structure
 		Hypercube hypercube(args.k, getFileLines(args.input_file)/4, getFileLineLength(args.input_file)-1);
 
 		hypercube.set_search_limits(args.probes, args.M, args.k);
-
-		std::cout << "Setted search limits" << std::endl;
-
 		hypercube.loadVectors(&input_vecs);
-		ShortedList* shorted_list = hypercube.search_hypercube(&input_vecs.array[0]);
 
-		// cube.analyze_query_vectors(&query_vecs);
+		for(unsigned i = 0; i < (query_vecs.size); i++)
+		{
+			Vector *query_vector = &((query_vecs.array)[i]);
+			timer.tic(); ShortedList* shorted_list = hypercube.search_hypercube(query_vector);
+			timer.tic(); hypercube_results = hypercube.k_nearest_neighbors_search(args.k);
+			timer.tic(); range_results = hypercube.range_search(args.R);
+
+			report_results(args.output_file, query_vector->id, hypercube_results, hypercube_time, range_results);
+		}
 
 		// Clear the old args
 		args.clear();
@@ -51,4 +67,43 @@ int main(int argc, char *argv[]){
 
 	print_footer();
 	return 0;
+}
+
+
+void report_results(std::string filename, unsigned id,
+	                  ShortedList *hypercube_results, double hypercube_time,
+	                  ShortedList *range_results)
+{
+	unsigned i=1;
+	std::ofstream file;
+
+	// Open the output file in append mode
+	file.open(filename, std::ios_base::app);
+
+	// Write the query id
+	file << "Query: " << id << std::endl;
+
+	// Write the results for each "i-th" Neighbor found (Comparing LSH and Naive-True)
+	SL_Node *hypercube_point = hypercube_results->first;
+	while( hypercube_point != nullptr ){
+		file << "Nearest neighbor-" << i++ << ": " << hypercube_point->v->id << std::endl;
+		file << "distanceHypercube:  "  << hypercube_point->dist   << std::endl;
+		// file << "distanceTrue: " << naive_p->dist << std::endl;
+
+		hypercube_point = hypercube_point->next;
+		// naive_p = naive_p->next;
+	}
+
+	// Write the test times
+	file << "tHypercube: "  << hypercube_time   << std::endl;
+	// file << "tTrue: " << naive_t << std::endl;
+
+	// Write the Range search results
+	file << "R-near neighbors:" << std::endl;
+	SL_Node *cur = range_results->first;
+	while( cur != nullptr ){
+		file << cur->v->id << std::endl;
+		cur = cur->next;
+	}
+		file << std::endl;
 }
